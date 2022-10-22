378e574dfcc099cfde1b8f3b5594c7380f1b15e5
CleiverSoares <cleiversoares2@gmail.com>
2022-10-22 00:49:55 -0300

    atualizaçoes de config de email, e end point

---
+++ Ecommerce/src/main/java/org/serratec/backend/projetoFinal/controller/EnderecoController.java
@@ -0,0 +1,89 @@
+package org.serratec.backend.projetoFinal.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+import javax.websocket.server.PathParam;
+
+import org.serratec.backend.projetoFinal.domain.Endereco;
+import org.serratec.backend.projetoFinal.service.EnderecoService;
+import org.serratec.backend.projetoFinal.service.EnderecoViaCepService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/endereco")
+public class EnderecoController {
+
+	@Autowired
+	private EnderecoService enderecoService;
+
+	@Autowired
+	private EnderecoViaCepService viaCepService;
+
+	@GetMapping
+	public ResponseEntity<List<Endereco>> listarTodos() {
+		Optional<List<Endereco>> endereco = enderecoService.listarTodosService();
+		if (endereco.isPresent()) {
+			return ResponseEntity.ok(endereco.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Endereco> listar(@PathVariable Long id) {
+		Optional<Endereco> endereco = enderecoService.listarService(id);
+		if (endereco.isPresent()) {
+			return ResponseEntity.ok(endereco.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> cadastrarEndereco(@Valid @RequestBody Endereco endereco) {
+		enderecoService.cadastrarService(endereco);
+		return ResponseEntity.status(201).build();
+	}
+
+	@PostMapping("/cadastrarPorCep")
+	public ResponseEntity<Endereco> cadastrarEnderecoPorCep(@PathParam("cep") String cep,
+			@PathParam("numero") String numero, @PathParam("complemento") String complemento) {
+		Endereco enderecoViaCepSite = viaCepService.buscarService(cep, numero);
+		if (enderecoViaCepSite != null) {
+			if (complemento != null) {
+				enderecoViaCepSite.setComplemento(complemento);
+			}
+			enderecoService.cadastrarService(enderecoViaCepSite);
+			return ResponseEntity.status(201).build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@PutMapping("/atualizar/{id}")
+	public ResponseEntity<Endereco> atualizar(@PathVariable Long id, @Valid @RequestBody Endereco dadosEndereco) {
+		Optional<Endereco> endereco = enderecoService.atualizarService(id, dadosEndereco);
+		if (!endereco.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(endereco.get());
+	}
+
+	@DeleteMapping("/deletar/{id}")
+	public ResponseEntity<Void> deletar(@PathVariable Long id) {
+		boolean foiDeletada = enderecoService.deletar(id);
+		if (!foiDeletada) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.noContent().build();
+	}
+}
---
+++ Ecommerce/src/main/java/org/serratec/backend/projetoFinal/controller/PedidoController.java
@@ -0,0 +1,107 @@
+package org.serratec.backend.projetoFinal.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.serratec.backend.projetoFinal.domain.Pedido;
+import org.serratec.backend.projetoFinal.service.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@RestController
+@RequestMapping("/api/pedido")
+public class PedidoController {
+
+	@Autowired
+	private PedidoService pedidoService;
+
+	@GetMapping
+	@ApiOperation(value = "Lista todos os pedidos", notes = "Listagem de Pedidos")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Retorna todos os pedidos"),
+			@ApiResponse(code = 401, message = "Erro de autenticação"),
+			@ApiResponse(code = 403, message = "Não há permissão para acessar o recurso"),
+			@ApiResponse(code = 404, message = "Recurso não encontrado"),
+			@ApiResponse(code = 505, message = "Exceção interna da aplicação"), })
+	public ResponseEntity<List<Pedido>> listarTodos() {
+		Optional<List<Pedido>> pedido = pedidoService.listarTodos();
+		if (pedido.isPresent()) {
+			return ResponseEntity.ok(pedido.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@GetMapping("/{id}")
+	@ApiOperation(value = "Retorna um pedido especifico", notes = "Listagem de Pedidos")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Retorna um pedido"),
+			@ApiResponse(code = 401, message = "Erro de autenticação"),
+			@ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+			@ApiResponse(code = 404, message = "Recurso não encotrado"),
+			@ApiResponse(code = 505, message = "Ocorreu uma exceção") })
+	public ResponseEntity<Pedido> listar(@PathVariable Long id) {
+		Optional<Pedido> pedido = pedidoService.listar(id);
+		if (pedido.isPresent()) {
+			return ResponseEntity.ok(pedido.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	@ApiOperation(value = "Salva um pedido")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Pedido Salvo"),
+			@ApiResponse(code = 401, message = "Erro de autenticação"),
+			@ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+			@ApiResponse(code = 404, message = "Recurso não encotrado"),
+			@ApiResponse(code = 505, message = "Ocorreu uma exceção") })
+	public ResponseEntity<Void> cadastrarPedido(@Valid @RequestBody Pedido pedido) {
+		boolean foiCadastrado = pedidoService.cadastrarPedido(pedido);
+		if (foiCadastrado) {
+			return ResponseEntity.status(201).build();
+		}
+		return ResponseEntity.internalServerError().build();
+	}
+
+	@PutMapping("/{id}")
+	@ApiOperation(value = "Atualiza um pedido especifico")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Pedido atualizado"),
+			@ApiResponse(code = 401, message = "Erro de autenticação"),
+			@ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+			@ApiResponse(code = 404, message = "Recurso não encotrado"),
+			@ApiResponse(code = 505, message = "Ocorreu uma exceção") })
+	public ResponseEntity<Pedido> atualizar(@PathVariable Long id, @Valid @RequestBody Pedido dadosPedido) {
+		Optional<Pedido> pedido = pedidoService.atualizar(id, dadosPedido);
+		if (!pedido.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(pedido.get());
+	}
+
+	@DeleteMapping("/{id}")
+	@ApiOperation(value = "Deleta um pedido especifico")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Pedido Deletado"),
+			@ApiResponse(code = 401, message = "Erro de autenticação"),
+			@ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+			@ApiResponse(code = 404, message = "Recurso não encotrado"),
+			@ApiResponse(code = 505, message = "Ocorreu uma exceção") })
+	public ResponseEntity<Void> deletar(@PathVariable Long id) {
+		boolean foiDeletado = pedidoService.deletar(id);
+		if (!foiDeletado) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.noContent().build();
+	}
+
+}
